c32a0d8abb1ffa96cb5641107b4549ccf7050dd1
Deran Schilling <derans@gmail.com>
2011-11-21 00:39:15 -0600

    Modified MultipleOfNumberReplacement

--- src/Interview.Core/Model/MultipleOfNumberReplacement.cs
+++ src/Interview.Core/Model/MultipleOfNumberReplacement.cs
@@ -2,14 +2,14 @@
 
 namespace Interview.Core.Model
 {
-    public class MultipleOfNumberReplacement : NumberReplacement
+   public class MultipleOfNumberReplacement : NumberReplacement
     {
         public MultipleOfNumberReplacement(int number, string text) : base(number, text)
         {}
 
         public override string GetTextFor(int number)
         {
-            return number.IsMultipleOf(Number) ? Text : null;
+            return number.IsMultipleOf(NumberWithText.Number) ? NumberWithText.Text : null;
         }
     }
 }
